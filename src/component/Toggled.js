466a32297f07ace01f7854a2356832b99e1a80a9
Helena-Bali <elenabaltajs@gmail.com>
2025-03-04 21:45:37 +0300

    changes-in-layout

--- src/component/Toggled.js
+++ src/component/Toggled.js
@@ -6,80 +6,79 @@ const Toggled = ({match}) => {
     return (
         <div className="toggled-parent">
             <div className="inner-toggled-parent">
-                <div className="flex justify-between">
-                    <div className="team-container">
-                        <div className="user-parent-cont">
-                            {match.awayTeam.players.map((player, i) => (
-                                <div key={i} className="user-container">
-                                    <div className="avatar-container">
-                                        <img
-                                            src={avatar}
-                                            alt="avatar"
-                                            className="avatar"/>
-                                        <span className="text-white">
+                <div className="team-container">
+                    <div className="user-parent-cont">
+                        {match.awayTeam.players.map((player, i) => (
+                            <div key={i} className="user-container">
+                                <div className="avatar-container">
+                                    <img
+                                        src={avatar}
+                                        alt="avatar"
+                                        className="avatar"/>
+                                    <span className="text-white">
                                             {player.username}
                                      </span>
-                                    </div>
-                                    <span className="murders-container">
-                                        Убийств: <span className="text-white">{player.kills}</span>
-                                    </span>
                                 </div>
-                            ))}
-                        </div>
-                        <div className="score-container">
-                            <div className="score-item">Points:
-                                <span className="score-number">
+                                <span className="murders-container">
+                                        Убийств:
+                                        <span className="text-white ml-1">{player.kills}</span>
+                                    </span>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="score-container">
+                        <div className="score-item">Points:
+                            <span className="score-number">
                                     {match.awayTeam.points}
                                 </span>
-                            </div>
-                            <div className="score-item">Место:
-                                <span className="score-number">
+                        </div>
+                        <div className="score-item">Место:
+                            <span className="score-number">
                                     {match.awayTeam.place}
                                 </span>
-                            </div>
-                            <div className="score-item">Всего убийств:
-                                <span className="score-number">
+                        </div>
+                        <div className="score-item">Всего убийств:
+                            <span className="score-number">
                                     {match.awayTeam.total_kills}
                                 </span>
-                            </div>
                         </div>
                     </div>
-                    <div className="team-container">
-                        <div className="user-parent-cont">
-                            {match.homeTeam.players.map((player, i) => (
-                                <div key={i} className="user-container">
-                                    <div className="avatar-container">
-                                        <img
-                                            src={avatar}
-                                            alt="avatar"
-                                            className="avatar"/>
-                                        <span className="text-white">
+                </div>
+                <div className="team-container">
+                    <div className="user-parent-cont">
+                        {match.homeTeam.players.map((player, i) => (
+                            <div key={i} className="user-container">
+                                <div className="avatar-container w-[48%]">
+                                    <img
+                                        src={avatar}
+                                        alt="avatar"
+                                        className="avatar"/>
+                                    <span className="text-white">
                                            {player.username}
                                      </span>
-                                    </div>
-                                    <span className="murders-container">
-                                        Убийств:
-                                        <span className="text-white ml-2">{player.kills}</span>
-                                    </span>
                                 </div>
-                            ))}
-                        </div>
-                        <div className="score-container">
-                            <div className="score-item">Points:
-                                <span className="score-number">
+                                <span className="murders-container">
+                                        Убийств:
+                                        <span className=" text-white ml-1">{player.kills}</span>
+                                    </span>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="score-container">
+                        <div className="score-item">Points:
+                            <span className="score-number">
                                     {match.homeTeam.points}
                                 </span>
-                            </div>
-                            <div className="score-item">Место:
-                                <span className="score-number">
+                        </div>
+                        <div className="score-item">Место:
+                            <span className="score-number">
                                     {match.homeTeam.place}
                                 </span>
-                            </div>
-                            <div className="score-item">Всего убийств:
-                                <span className="score-number">
+                        </div>
+                        <div className="score-item">Всего убийств:
+                            <span className="score-number">
                                     {match.homeTeam.total_kills}
                                 </span>
-                            </div>
                         </div>
                     </div>
                 </div>
